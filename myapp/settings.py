2d2c4a652e7731ea4713a87166e8b14c9f790602
ken <19k1e1n04@gmail.com>
2022-02-02 16:59:35 +0900

    initial commit

--- myapp/settings.py
+++ myapp/settings.py
@@ -54,7 +54,7 @@ EMAIL_HOST_USER = 'info.geeknagoya@gmail.com'
 EMAIL_HOST_PASSWORD = 'geeknagoya1031'
 EMAIL_USE_TLS = True
 
-LOGIN_URL = 'accounts:login'
+LOGIN_URL = 'accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
