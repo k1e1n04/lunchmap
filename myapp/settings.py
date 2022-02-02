417ca65960e812aa8ce545336bcce2d3aaaa5a81
ken <19k1e1n04@gmail.com>
2022-02-02 14:31:54 +0900

    initial commit

--- myapp/settings.py
+++ myapp/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
