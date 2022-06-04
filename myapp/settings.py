69f4e3a455529cd86a1c3e3cda59ec2b2eaefc2d
Ken Ishii <riguveda44@gmail.com>
2022-06-05 00:32:56 +0900

    パスワード削除

--- myapp/settings.py
+++ myapp/settings.py
@@ -94,18 +94,7 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         # 'ENGINE': 'django.db.backends.sqlite3',
-#         # 'NAME': BASE_DIR / 'db.sqlite3',
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'geek_lunchmap',
-#         'USER': 'root',
-#         'PASSWORD': 'Gh63tdd31%',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#     }
-# }
+
 
 DATABASES = {
     'default': {
